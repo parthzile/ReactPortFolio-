1f2d7a901abcf4b2a127579b31efe0c40fc52b22
parthzile <parthzile01@gmail.com>
2024-04-10 16:27:54 +0530

    Added decode text component

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,6 +2,8 @@ import styles from "./Landing.module.css";
 import content from "../content.json";
 import Animation from "../Animation/Animation";
 import { useState,useEffect } from "react";
+// import DecodeText from "../../components/DecodeText/DecodeText"
+
 
 const name = content.landingSection.name.toUpperCase();
 const titleOne = content.landingSection.titleOne;
@@ -26,6 +28,7 @@ const Landing = () => {
 
   return (
     <div className={styles.landing}>
+      {/* <DecodeText text="HELLO WROLD"/> */}
       <div className={styles.name}>{name}</div>
       <div className={styles.titleContainer}>
         <div className={styles.titleOne}>{titleOne}</div>
